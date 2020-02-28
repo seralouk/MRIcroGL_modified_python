2fbe193c11714223459a8e1f201f742136981c83
Serafeim LOUKAS <seralouk@gmail.com>
2020-02-28 23:46:57 +0100

    more commenting

--- MRIcroGL_rotating_movie.py
+++ MRIcroGL_rotating_movie.py
@@ -5,7 +5,7 @@ import gl
 gl.resetdefaults()
 
 #* Where to save results
-save_to = '/Users/loukas/Desktop/MRIcroGL_movie/results/' # do not forget the last slash
+save_to = '/Users/loukas/Documents/MRIcroGL_movie/results/' # do not forget the last slash
 
 #* Set rendering view mode
 gl.view(64)
@@ -14,7 +14,7 @@ gl.view(64)
 gl.loadimage('spm152')
 
 #* Open overlay: show positive regions
-gl.overlayload('/Users/loukas/Desktop/AAL_masked_region_32.nii')
+gl.overlayload('/Users/loukas/Documents/MRIcroGL_movie/AAL_masked_region_32.nii')
 #gl.minmax(1, 4, 4)
 #gl.opacity(1,50)
 
@@ -26,10 +26,14 @@ x0=1 # from
 x = 360 # to 
 step = 20 # with step
 for x in range(x0,x,step):
-  gl.azimuthelevation(x, 0);
+  gl.azimuthelevation(x, 0)
+  #* 'wait' command is optional
   gl.wait(10)
+  #* change resolution
+  gl.bmpzoom(0.5)
+  #* Save rotated rendered brain views
   gl.savebmp(save_to + 'rot_' + str(x) + '.png')
 
-#* Make it a gif using imagemagick¨
+#* Create a gif using imagemagick or similar software¨
 #* Call the following commad from the terminal after CDing into 'save_to' directory
-#convert -delay 90 *rot_* rota_brain.gif
+#convert -delay 90 *rot_* rotating_brain.gif
